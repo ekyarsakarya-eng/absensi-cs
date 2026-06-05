5a837c1caa99acc94f61d0e05b6e9a077d9dc7ae
Eko Didie Yuliantho <eky.arsakarya@gmail.com>
2026-06-06 06:08:33 +0700

    Refactor service worker for new cache version
    Updated service worker to use a new cache version and simplified event listeners.

--- sw.js
+++ sw.js
@@ -1,44 +1,4 @@
-const CACHE = 'absensi-v24';
-const ASSETS = [
-  './',
-  './index.html',
-  './app.js',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png',
-  './icon-absensi.png',
-  './icon-rekap.png',
-  './icon-profile.png'
-];
-
-// Install - cache file inti
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS))
-  );
-  self.skipWaiting();
-});
-
-// Activate - hapus cache lama
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => 
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-// Fetch - utamakan cache, fallback ke network
-self.addEventListener('fetch', e => {
-  if (e.request.method !== 'GET') return;
-  e.respondWith(
-    caches.match(e.request).then(cached => {
-      return cached || fetch(e.request).then(res => {
-        const copy = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, copy));
-        return res;
-      }).catch(() => caches.match('./index.html'));
-    })
-  );
-});
+const CACHE='absensi-v26';
+const ASSETS=['./','./index.html','./app.js','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
